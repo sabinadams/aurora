14313b58692dabea82d2f84f1cfc2506cdfe5fb0
Sabin Adams <sabintheworld@gmail.com>
2021-12-09 23:26:05 -0800

    Grabs enums, generators, datasources, models

--- src/aurora.ts
+++ src/aurora.ts
@@ -1,12 +1,47 @@
 import { parseSchema, getAuroraConfigJSON } from './functions';
-import path from 'path';
 import { AuroraConfig, SchemaInformation } from './models';
+import type { DMMF } from "@prisma/client/runtime";
+import type { DataSource, GeneratorConfig } from "@prisma/generator-helper";
+import { ERRORS } from './util/CONSTANTS';
 
 export default async function aurora() {
   // Grab the aurora configuration options from config file
   const config: AuroraConfig = await getAuroraConfigJSON()
 
   // Parse out the information from each prisma file
-  const schemaConfigs: SchemaInformation[] = await Promise.all( config.files.map( parseSchema ))
+  const schemas: SchemaInformation[] = await Promise.all( config.files.map( parseSchema ) )
 
+  // Get a list of all the models
+  const models: DMMF.Model[] = schemas.reduce((acc: DMMF.Model[], curr: SchemaInformation) => [...acc, ...curr.models], [])  
+
+  // Mix the models
+
+  // Get all the enums
+  const enums: DMMF.DatamodelEnum[] = schemas.reduce((acc: DMMF.DatamodelEnum[], curr: SchemaInformation) => [...acc, ...curr.enums], [])  
+  
+  // Get all the datasources ( check if multiple non-unique. If so, error )
+  let allDatasources: DataSource[] = schemas.reduce((acc: DataSource[], curr: SchemaInformation) => [...acc, ...curr.datasources], [])  
+  const datasources = Array.from(new Set(allDatasources))
+  if ( datasources.length > 1 ) {
+    console.error(
+      `There were ${datasources.length} different datasources provided. Make sure all of the datasources are the same.`
+    )
+    throw new Error(ERRORS.INVALID_SCHEMA)
+  }
+
+  // We can now get the singular datasource. Will be funciton call later
+  const datasource = datasources[0]
+
+  // Get all the generators (check if multiple non-unique. If so, error )
+  let allGenerators: GeneratorConfig[] = schemas.reduce((acc: GeneratorConfig[], curr: SchemaInformation) => [...acc, ...curr.generators], [])  
+  const generators = Array.from(new Set(allGenerators))
+  if ( generators.length > 1 ) {
+    console.error(
+      `There were ${generators.length} different generators provided. Make sure all of the generators are the same.`
+    )
+    throw new Error(ERRORS.INVALID_SCHEMA)
+  }
+
+  // We can now get the singular generator. Will be funciton call later
+  const generator = generators[0]
 }
--- src/util/CONSTANTS.ts
+++ src/util/CONSTANTS.ts
@@ -3,5 +3,6 @@ export const ERRORS = {
     INVALID_CONFIG_FILE: 'Invalid Config File',
     EMPTY_CONFIG_FILES: 'No Schema Files Defined',
     NO_OUTPUT_CONFIGURED: 'No Output Location Provided',
-    NON_PRISMA_FILE: 'Non-Prisma file provided'
+    NON_PRISMA_FILE: 'Non-Prisma file provided',
+    INVALID_SCHEMA: 'There was an issue with at least one of your schemas'
 }
