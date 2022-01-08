811f84ab9e40a47674dedb2a8a11a7178fd3b7d9
Sabin Adams <sabintheworld@gmail.com>
2022-01-08 02:46:22 -0800

    Adds tests for Models Fields

--- src/tests/aurora.test.ts
+++ src/tests/aurora.test.ts
@@ -182,13 +182,151 @@ describe('aurora()', () => {
             describe('@@ignore', () => {
                 it('should NOT render an @@ignore model', async () => {
                     const generatedSchema = await getGeneratedSchema(['feature-specific/models/model-@@ignore.prisma'])
-                    expect(generatedSchema).not.toContain('@@ignore')
+                    expect(generatedSchema).not.toContain('model User')
                 })
             })
         })
 
         describe('Model Fields', () => {
 
+            it('should render ?', async () => {
+                const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-?.prisma'])
+                expect(generatedSchema).toContain('date DateTime?')
+            })
+
+            it('should render []', async () => {
+                const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-[].prisma'])
+                expect(generatedSchema).toContain('data String[]')
+            })
+
+            describe('@id', () => {
+                it('should render @id', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@id.prisma'])
+                    expect(generatedSchema).toContain('id Int @id')
+                })
+
+                it('should render @id(map: "")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@id-map.prisma'])
+                    expect(generatedSchema).toContain('id Int @id(map: "test")')
+                })
+
+                it('should render @id(length: 1)', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@id-length.prisma'])
+                    expect(generatedSchema).toContain('id String @id(length: 1)')
+                })
+
+                it('should render @id(sort: Asc)', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@id-sort.prisma'])
+                    expect(generatedSchema).toContain('id String @id(sort: Asc)')
+                })
+            })
+
+            describe('@default', () => {
+                it('should render @default("")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@default.prisma'])
+                    expect(generatedSchema).toContain('id String @id @default("test")')
+                })
+
+                it('should render @default(value: "")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@default-value.prisma'])
+                    expect(generatedSchema).toContain('id String @id @default(value: "test")')
+                })
+
+                it('should render @default(map: "", "")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@default-map.prisma'])
+                    expect(generatedSchema).toContain('id String @id @default(map: "mapping", "test")')
+                })
+            })
+
+            describe('@unique', () => {
+                it('should render @unique', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@unique.prisma'])
+                    expect(generatedSchema).toContain('id Int @id @unique')
+                })
+
+                it('should render @unique(map: "")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@unique-map.prisma'])
+                    expect(generatedSchema).toContain('id Int @id @unique(map: "mapping")')
+                })
+
+                it('should render @unique(length: 1)', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@unique-length.prisma'])
+                    expect(generatedSchema).toContain('id String @unique(length: 1)')
+                })
+
+                it('should render @unique(sort: Asc)', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@unique-sort.prisma'])
+                    expect(generatedSchema).toContain('id String @unique(sort: Asc)')
+                })
+            })
+
+            describe('@relation', () => {
+                it('should render @relation("")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation.prisma'])
+                    expect(generatedSchema).toContain('@relation("UserPost")')
+                })
+
+                it('should render @relation(fields: [])', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation.prisma'])
+                    expect(generatedSchema).toContain('fields: [id]')
+                })
+
+                it('should render @relation(references: [])', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation.prisma'])
+                    expect(generatedSchema).toContain('references: [id]')
+                })
+
+                it('should render @relation(name: "", fields: [], references: [])', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation-name.prisma'])
+                    expect(generatedSchema).toContain('@relation(name: "UserPost", fields: [id], references: [id])')
+                })
+
+                it('should render @relation(fields: [], references: [])', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation-unnamed.prisma'])
+                    expect(generatedSchema).toContain('@relation(fields: [id], references: [id])')
+                })
+
+                it('should render @relation(onDelete: action)', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation-ondelete.prisma'])
+                    expect(generatedSchema).toContain('@relation(fields: [id], references: [id], onDelete: Cascade)')
+                })
+
+                it('should render @relation(onUpdate: action)', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation-onupdate.prisma'])
+                    expect(generatedSchema).toContain('@relation(fields: [id], references: [id], onUpdate: Cascade)')
+                })
+
+                it('should render @relation(map: "")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@relation-map.prisma'])
+                    expect(generatedSchema).toContain('@relation(map: "Test", fields: [id], references: [id])')
+                })
+            })
+
+            describe('@map', () => {
+                it('should render @map("")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@map.prisma'])
+                    expect(generatedSchema).toContain('id String @id @map("test")')
+                })
+
+                it('should render @map(name: "")', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@map-name.prisma'])
+                    expect(generatedSchema).toContain('id String @id @map(name: "test")')
+                })
+            })
+
+            describe('@updatedAt', () => {
+                it('should render @updatedAt', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@updatedAt.prisma'])
+                    expect(generatedSchema).toContain('date DateTime @updatedAt')
+                })
+            })
+
+            describe('@ignore', () => {
+                it('should NOT render @ignore', async () => {
+                    const generatedSchema = await getGeneratedSchema(['feature-specific/model-fields/model-field-@ignore.prisma'])
+                    expect(generatedSchema).not.toContain('name String')
+                })
+            })
         })
     })
 });
