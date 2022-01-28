c02fe935e7e3d286bbe81bd4d1a08b69fbb4bbde
Tamal Patra <patra.tamal@gmail.com>
2022-01-28 13:36:37 +0530

    build new package

--- lib/helpers/index.d.ts
+++ lib/helpers/index.d.ts
@@ -2,4 +2,5 @@ import { parseSchema } from './parseSchema';
 import { getAuroraConfigJSON } from './getAuroraConfigJson';
 import { combineModels } from './combineModels';
 import { writeSchema } from './writeSchema';
-export { parseSchema, getAuroraConfigJSON, combineModels, writeSchema };
+import { expandGlobPatterns } from './expandGlobPatterns';
+export { parseSchema, getAuroraConfigJSON, combineModels, writeSchema, expandGlobPatterns };
