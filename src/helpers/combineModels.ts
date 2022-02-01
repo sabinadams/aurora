a1ea69a82c994b1df788bd6d88ef6ebc650a4b54
Sabin Adams <sabintheworld@gmail.com>
2022-02-01 00:43:52 -0800

    Enhancement/merge duplicate attributes (#22)
    * Merges duplicated fields' attributes
    
    * Adds logic to merge duplicate fields
    
    * Bumps version
    
    * Ran build

--- src/helpers/combineModels.ts
+++ src/helpers/combineModels.ts
@@ -16,6 +16,35 @@ export function combineModels(rawModels: DMMF.Model[]): DMMF.Model[] {
             // If we don't already have this field
             if (!fields.map((field) => field.name).includes(field.name)) {
               fields.push(field);
+            } else {
+              const indexOfExisting = fields.findIndex(
+                (existingField) => existingField.name === field.name
+              );
+              const existing = fields.find((existingField) => existingField.name === field.name);
+              // Combine the attributes
+              fields[indexOfExisting].attributes = Array.from(
+                new Set([...fields[indexOfExisting].attributes, ...field.attributes])
+              );
+
+              // Get a list of all of the old and new
+              let attributes = [...existing.attributesFixed, ...field.attributesFixed];
+              // Combine them and merge attribute options (some data can be lost here)
+              attributes = Array.from(
+                new Set(
+                  attributes.map((attr) => {
+                    // Get a list of all versions of this attribute
+                    const likeAttributes = attributes.filter(
+                      (likes) => attr.attributeType === likes.attributeType
+                    );
+                    // Merge (can get rid of some conflicting options)
+                    attr = Object.assign(attr, ...likeAttributes);
+                    // We want a string so we can get unique options in a set
+                    return JSON.stringify(attr);
+                  })
+                )
+              ).map((attr) => JSON.parse(attr)); // Now we can parse that string and get JSON again
+
+              fields[indexOfExisting].attributesFixed = attributes;
             }
             return fields;
           },
@@ -44,6 +73,7 @@ export function combineModels(rawModels: DMMF.Model[]): DMMF.Model[] {
         // Add the model to our record
         acc[curr.name] = curr;
       }
+
       return acc;
     },
     {}
