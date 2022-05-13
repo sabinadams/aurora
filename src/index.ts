1b9092c9f30de00347ca85ebc0fd85bfdb4841b7
Sabin Adams <sabintheworld@gmail.com>
2022-05-13 15:43:21 -0700

    Enhancement/output cleanup (#124)
    * Removes unnecessary dumps
    
    * Updates packages
    
    * Fixes build
    
    * Removes more unnecessary stuff
    
    * Fixes action

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-import { default as chalk } from 'chalk';
-import clear from 'clear';
-import * as figlet from 'figlet';
 import { Command } from 'commander';
 import aurora from './aurora';
 import dotenv from 'dotenv';
@@ -10,18 +7,9 @@ import dotenv from 'dotenv';
 // Get Env Vars
 dotenv.config();
 
-// Console Welcome
-clear();
-console.log(chalk.bold.magenta(figlet.textSync('Aurora', { horizontalLayout: 'full' })));
-
 // CLI Command
 const program = new Command();
-program
-  .version('1.3.3')
-  .description(
-    'A tool that orchestrates prisma files in a way that allows multiple .prisma files with cross-relations'
-  )
-  .parse(process.argv);
+program.parse(process.argv);
 
 // Run aurora
 aurora();
